aec295295d77593a4594eedb8d3a2807794ea59c
Chris <panique@users.noreply.github.com>
2016-08-25 21:01:18 +0200

    "moved" homeController from lowercase controller folder to camelcase controller folder

---
+++ application/Controller/HomeController.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Class HomeController
+ *
+ * Please note:
+ * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
+ * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
+ *
+ */
+
+namespace Mini\Controller;
+
+class HomeController
+{
+    /**
+     * PAGE: index
+     * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
+     */
+    public function index()
+    {
+        // load views
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/home/index.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    /**
+     * PAGE: exampleone
+     * This method handles what happens when you move to http://yourproject/home/exampleone
+     * The camelCase writing is just for better readability. The method name is case-insensitive.
+     */
+    public function exampleOne()
+    {
+        // load views
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/home/example_one.php';
+        require APP . 'view/_templates/footer.php';
+    }
+
+    /**
+     * PAGE: exampletwo
+     * This method handles what happens when you move to http://yourproject/home/exampletwo
+     * The camelCase writing is just for better readability. The method name is case-insensitive.
+     */
+    public function exampleTwo()
+    {
+        // load views
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/home/example_two.php';
+        require APP . 'view/_templates/footer.php';
+    }
+}
