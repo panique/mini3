081d6b0c70dc318abdfd58d8d9c69fd8a15f4c30
panique <panique@web.de>
2022-09-13 01:40:05 +0200

    2022 update: PHP8.1, Ubuntu 22.04, nginx, easier setup

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -6,7 +6,7 @@
 
     <!-- jQuery, loaded in the recommended protocol-less way -->
     <!-- more http://www.paulirish.com/2010/the-protocol-relative-url/ -->
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//code.jquery.com/jquery-3.6.1.min.js"></script>
 
     <!-- define the project's URL (to make AJAX calls possible, even when using this in sub-folders etc) -->
     <script>
